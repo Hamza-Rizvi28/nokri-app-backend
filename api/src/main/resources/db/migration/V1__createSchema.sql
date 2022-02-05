2026dd4e4a34015d79e55d9cd5e95cb78afd9ab1
mustufain <mustufain.rizvi@disqo.com>
2022-02-05 21:09:58 +0200

    setup flyway for database migrations.

---
+++ api/src/main/resources/db/migration/V1__createSchema.sql
@@ -0,0 +1,73 @@
+create table "user" (
+    user_id bigserial not null primary key,
+    user_name varchar(128) not null,
+    email varchar(128) not null,
+    "password" varchar(128) not null,
+    last_logged_in date,
+    email_verified boolean,
+    active boolean,
+    "role" int
+);
+
+create table job (
+        job_id bigserial not null primary key,
+        descripton text not null,
+        country varchar(128) not null,
+        city varchar(128) not null,
+        contract_type varchar(128) not null,
+        seniority_level varchar(128) not null,
+        title varchar(128) not null,
+        active boolean not null
+);
+
+create table user_job (
+        user_id bigint not null,
+        job_id bigint not null,
+        primary key(user_id,
+job_id),
+        constraint fk_user foreign key(user_id) references "user"(user_id),
+        constraint fk_job foreign key(job_id) references job(job_id)
+);
+
+create table userprofile (
+        profile_id bigserial not null primary key,
+        user_id bigserial not null,
+        objective varchar(256),
+        country varchar(128),
+        city varchar(128),
+        street varchar(128),
+        zipcode int,
+        linkedin_url varchar(128),
+        country_code varchar(128),
+        contact_no int,
+        constraint fk_user foreign key(user_id) references "user"(user_id)
+);
+
+create table education (
+        education_id bigserial not null primary key,
+        profile_id bigserial not null,
+        education_level varchar(128) not null,
+        university varchar(128),
+        "degree" varchar(128),
+        from_date text,
+        "to_date" text,
+        constraint fk_profile foreign key(profile_id) references userprofile(profile_id)
+);
+
+create table project (
+        project_id bigserial not null primary key,
+        profile_id bigserial not null,
+        descripton text not null,
+        constraint fk_profile foreign key(profile_id) references userprofile(profile_id)
+);
+
+create table work_experience (
+        work_experience_id bigserial not null primary key,
+        profile_id bigserial not null,
+        from_date text not null,
+        "to_date" text not null,
+        organization varchar(128) not null,
+        designation varchar(128) not null,
+        descripton text,
+        constraint fk_profile foreign key(profile_id) references userprofile(profile_id)
+);
